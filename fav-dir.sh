3269b25c22579ee5114d7a0dad48e205508e80d8
John Grib <johngrib82@gmail.com>
2018-09-24 20:40:37 +0900

    Bug fix: does not change directory when selected none

--- fav-dir.sh
+++ fav-dir.sh
@@ -1,9 +1,18 @@
 function fav() {
     if [ "$1" = "" ]; then
-        cd $(grep -e 'cd\s' ~/.vim/session/* ~/.fav-dir 2>/dev/null | sed "s/^.*:cd //" | sed "s,~,$HOME," | uniq | fzf )
+
+        FAV_TARGET_PATH=$(grep -e 'cd\s' ~/.vim/session/* ~/.fav-dir 2>/dev/null | sed "s/^.*:cd //" | sed "s,~,$HOME," | uniq | fzf )
+
+        if [ "$FAV_TARGET_PATH" != "" ]; then
+            cd $FAV_TARGET_PATH
+        fi
+
         return
+
     elif [ "$1" = "add" ]; then
+
         echo "add!!! $(pwd)"
         echo "cd $(pwd)" >> ~/.fav-dir
+
     fi
 }

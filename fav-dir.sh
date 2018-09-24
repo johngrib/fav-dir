2714b59b1e7a496422f56bb4916af9e7d430cab3
John Grib <johngrib82@gmail.com>
2018-09-25 00:23:28 +0900

    Refactor: extract functions to create lists

--- fav-dir.sh
+++ fav-dir.sh
@@ -4,6 +4,9 @@ function fav() {
         FAV_TMP_FILE="/tmp/fav-dir-tempfile"
 
         fav_create_list
+        fav_create_vim_session_dir_list
+        fav_create_separate_line
+        fav_create_favorite_list
 
         FAV_TARGET_PATH=$(cat $FAV_TMP_FILE | grep -v '\-\-\-' | fzf )
         FAV_TARGET_PATH=$(echo $FAV_TARGET_PATH | sed "s/\[.*\] //" | sed "s,^~,$HOME,")
@@ -22,6 +25,9 @@ function fav() {
     elif [ "$1" = "list" ]; then
 
         fav_create_list
+        fav_create_vim_session_dir_list
+        fav_create_separate_line
+        fav_create_favorite_list
         cat $FAV_TMP_FILE
         echo '' > $FAV_TMP_FILE
     fi
@@ -29,8 +35,16 @@ function fav() {
 
 function fav_create_list() {
     echo '' > $FAV_TMP_FILE
+}
 
-    grep -e 'cd\s' ~/.vim/session/* | sed "s/^.*:cd /[VimSession] /" > $FAV_TMP_FILE 2>/dev/null
+function fav_create_vim_session_dir_list() {
+    grep -e 'cd\s' ~/.vim/session/* | uniq | sed "s/^.*:cd /[VimSession] /" >> $FAV_TMP_FILE 2>/dev/null
+}
+
+function fav_create_separate_line() {
     echo '------------' >> $FAV_TMP_FILE
-    cat ~/.fav-dir | sed -e "s/^/[ Favorite ] /" >> $FAV_TMP_FILE 2>/dev/null
+}
+
+function fav_create_favorite_list() {
+    cat ~/.fav-dir | uniq | sed -e "s/^/[ Favorite ] /" >> $FAV_TMP_FILE 2>/dev/null
 }

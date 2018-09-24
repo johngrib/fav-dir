83e1c69146fa794f21c93f62f1e31888a67b4405
John Grib <johngrib82@gmail.com>
2018-09-24 20:34:44 +0900

    Bug fix: silence error log when not exist .fav-dir

--- fav-dir.sh
+++ fav-dir.sh
@@ -1,6 +1,6 @@
 function fav() {
     if [ "$1" = "" ]; then
-        cd $(grep -e 'cd\s' ~/.vim/session/* ~/.fav-dir | sed "s/^.*:cd //" | sed "s,~,$HOME," | uniq | fzf )
+        cd $(grep -e 'cd\s' ~/.vim/session/* ~/.fav-dir 2>/dev/null | sed "s/^.*:cd //" | sed "s,~,$HOME," | uniq | fzf )
         return
     elif [ "$1" = "add" ]; then
         echo "add!!! $(pwd)"

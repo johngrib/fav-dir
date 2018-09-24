0afc794a87530a93f49db76f61733247205049b0
John Grib <johngrib82@gmail.com>
2018-09-25 00:51:45 +0900

    Add feature: rm

--- fav-dir.sh
+++ fav-dir.sh
@@ -30,6 +30,22 @@ function fav() {
         fav_create_favorite_list
         cat $FAV_TMP_FILE
         echo '' > $FAV_TMP_FILE
+
+    elif [ "$1" = "rm" ]; then
+
+        fav_create_list
+        fav_create_favorite_list
+
+        FAV_TARGET_PATH=$(cat $FAV_TMP_FILE | grep -v '\-\-\-' | fzf )
+        FAV_TARGET_PATH=$(echo $FAV_TARGET_PATH | sed "s/\[.*\] //" | sed "s,^~,$HOME,")
+
+        if [ "$FAV_TARGET_PATH" != "" ]; then
+            FAV_TMP_FILE="/tmp/fav-dir-tempfile"
+            grep -v -e "$FAV_TARGET_PATH\s*$" ~/.fav-dir | uniq > $FAV_TMP_FILE
+            cat $FAV_TMP_FILE > ~/.fav-dir
+            echo "removed : $FAV_TARGET_PATH"
+            return
+        fi
     fi
 }
 

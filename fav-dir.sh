13ce8acea45ea1c47037ba2877e3f7ded25cdc07
John Grib <johngrib82@gmail.com>
2018-09-24 20:32:07 +0900

    Add feature: add pwd

--- fav-dir.sh
+++ fav-dir.sh
@@ -1,6 +1,9 @@
 function fav() {
     if [ "$1" = "" ]; then
-        cd $(grep -e 'cd\s' ~/.vim/session/* ~/.fav-dir | sed "s/^.*:cd //" | sed "s,~,$HOME," | fzf )
+        cd $(grep -e 'cd\s' ~/.vim/session/* ~/.fav-dir | sed "s/^.*:cd //" | sed "s,~,$HOME," | uniq | fzf )
         return
+    elif [ "$1" = "add" ]; then
+        echo "add!!! $(pwd)"
+        echo "cd $(pwd)" >> ~/.fav-dir
     fi
 }
